46ed16ae44df6f278bce187f38a3a0c3fbf40c2c
RD107924 <randyhuang1007@gmail.com>
2025-06-29 13:31:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  // 禁用或啟用滾動，防止選單打開時頁面滾動
+  if (navMenu.classList.contains("active")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "";
+  }
+});
+
+// 點擊選單項目後關閉選單並允許滾動
+document.querySelectorAll(".nav-menu li a").forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    document.body.style.overflow = "";
+  });
+});
